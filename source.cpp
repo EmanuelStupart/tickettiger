9fe15090962f23a6fab0ffe4a7cd6cab317a64ee
David Kislyak <kislyakdavid@gmail.com>
2019-05-27 20:08:40 -0700

    Add c++ main template.
    Add the main file for the Ticket Tiger with necessary copyright and main function.

---
+++ source.cpp
@@ -0,0 +1,17 @@
+//
+//  source.cpp
+//  TicketTiger
+//
+//  Created by David Kislyak on 5/27/19.
+//  Copyright © 2019:
+//  David Kislyak, Emanuel Stupart, Liumu Ye, Rodney Deacon
+//  All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    
+}
